f105f0208a2831cd644505eae004f419be1c8f87
Nicole Apono <napononicole@gmail.com>
2024-05-01 12:02:23 +0300

    Update changes made to ProjectItem.js

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
-  console.log(name, about, technologies); // Ensure props are received correctly
+  console.log(name, about, technologies); 
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* Check if technologies array exists before using map */}
         {technologies && technologies.map((technology, index) => (
           <span key={index}>{technology}</span>
         ))}
